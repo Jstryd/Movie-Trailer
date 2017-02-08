641da865334d9563d50cd778be33aee535efb23f
Jstryd <jstryd@vcd-ch204-07.eastern.ewu.edu>
2017-02-08 13:42:02 -0800

    v2.0 NEW START
    Starting from new Template :D

--- README.md
+++
@@ -1,9 +0,0 @@
-# The Hobbit
-
-## Content Priority Checklist
-
-1. Trailer
-2. Title
-3. Release Date
-4. Buy Tickets
-5. See Movie
--- js/scripts.js
+++
@@ -1,6 +0,0 @@
-// Scripts!
-$('h1').lettering();
-// FitVids
-$('video').fitVids();
-// Video Player - not working yet
-$('video').mediaElement();
